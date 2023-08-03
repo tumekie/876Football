d773c30755898f2605f03936747aa059a9023b7a
tumekie1999 <tb000223@sbuniv.edu>
2023-08-02 20:31:17 -0500

    got date picker to work

--- screens/matches.js
+++ screens/matches.js
@@ -3,14 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, ScrollView, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
 import DatePicker from 'react-native-modern-datepicker';
 
-
 // get the current day in the format of the API 'YYYYMMDD'
 const date = new Date();
-
 let day = (date.getDate() + 1).toString().padStart(2, '0'); // Adding 1 to get the following day
 let month = (date.getMonth() + 1).toString().padStart(2, '0'); // Adding 1 to get the correct month index
 let year = date.getFullYear().toString();
-
 let fullDate = year.concat(month, day);
 
 // Dictionary to map leagueIds to image paths
@@ -33,20 +30,17 @@ const leagueImageMap = {
 const Matches = () => {
   const [fixtures, setFixtures] = useState([]);
   const [open, setOpen] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(null);
-
-  useEffect(() => {
-    fetchFixtures(fullDate); // Fetch fixtures for the current day initially
-  }, []);
+  const [selectedDate, setSelectedDate] = useState(fullDate);
 
   useEffect(() => {
     if (selectedDate) {
       //console.log("SELECTED DATE:", selectedDate);
-      fetchFixtures(formatDate(selectedDate)); // Fetch fixtures when the user selects a different date
+      const formattedDate = selectedDate.split('/').join(''); // format the selected date from the user to API format
+      //console.log("Formated DATE:", formattedDate);
+      fetchFixtures(formattedDate); // Fetch fixtures when the user selects a different date   
     }
   }, [selectedDate]);
   
-
   const fetchFixtures = async (dateParam) => {
     const options = {
       method: 'GET',
@@ -64,6 +58,7 @@ const Matches = () => {
 
     try {
       const response = await axios.request(options);
+      //console.log(response.data.results);
       const filteredFixtures = response.data.results.filter((fixture) =>
         [
           '10037429', '10041424', '10077222', '10069418', '10047168', '10050234', '10078898',
@@ -77,29 +72,19 @@ const Matches = () => {
     }
   };
 
-  const handleOnPress = () => {
+  function handleOnPress () {
     setOpen(!open);
   };
 
-  const handleChange = (propDate) => {
-    console.log('Selected Date:', propDate);
+  function handleChange (propDate) {
     setSelectedDate(propDate);
     setOpen(false); // Close the modal after selecting the date
-  };
-
-  // Helper function to format the date as "yyyymmdd"
-  const formatDate = (dateString) => {
-    const dateObject = new Date(dateString);
-    const year = dateObject.getFullYear();
-    const month = String(dateObject.getMonth() + 1).padStart(2, '0');
-    const day = String(dateObject.getDate()).padStart(2, '0');
-    return `${year}${month}${day}`;
-  };
+  };  
 
   return (
     <ScrollView style={styles.container}>
       <TouchableOpacity onPress={handleOnPress}>
-        <Text style={styles.date}>Date</Text>
+        <Text style={styles.date}>Calendar</Text>
       </TouchableOpacity>
       <Modal
         animationType='slide'
@@ -111,7 +96,7 @@ const Matches = () => {
             <DatePicker
               mode='calendar'
               selected={selectedDate}
-              onDateChanged={handleChange}
+              onDateChange={handleChange}
             />
             <TouchableOpacity onPress={handleOnPress}>
               <Text>Close</Text>
